c7664f7f0487f2b6208e3becce4d81ee3f3117c3
mozta <perezaguirrerafael@gmail.com>
2024-10-21 17:42:06 -0600

    add docs

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import json
 
 app = Flask(__name__)
 
+
 # Cargar los ítems del menú desde un archivo JSON
 def load_menu_items():
-    with open('menu.json', 'r', encoding='utf-8') as file:
-        return json.load(file)
+    try:
+        with open('menu.json', 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {"error": "File not found"}
+    except json.JSONDecodeError:
+        return {"error": "Error decoding JSON"}
 
 @app.route('/')
 def home():
-    return "Welcome to the API of the Menu of the Restaurant 🌮"
+    return render_template('index.html')
 
 
 # Ruta para obtener todos los ítems del menú
@@ -27,6 +33,14 @@ def get_menu_item(item_id):
     if item is None:
         return jsonify({"error": "Item not found"}), 404
     return jsonify(item), 200
+
+# Easter Egg oculto
+@app.route('/software', methods=['GET'])
+def secret():
+    return jsonify({
+        "message": "¡Felicidades! Has encontrado un Easter Egg 🌮🎉",
+        "question": "¿Qué sucede si se pasa una lista vacía como segundo argumento en useEffect?"
+    }), 200
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
